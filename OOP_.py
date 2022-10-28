c74add6811a20c3f028dc4fc2882a38fbfae0385
Dhotei Belikov <dhotei@ya.ru>
2022-10-28 09:09:43 +0300

    added comparison and objects

--- OOP_.py
+++ OOP_.py
@@ -8,12 +8,12 @@ class Student:
         self.grades = {}
         self.average_rate_1 = []
 
+
     def add_courses(self, course_name):
         self.finished_course.append(course_name)
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer,
-                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -23,6 +23,21 @@ class Student:
 
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average_rate_1}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}"
+
+    def __lt__(self, other: 'Student'):
+        if not isinstance(other, Student):
+            raise TypeError("Wrong type for comparison with Student Class", other.__class__)
+        return self.average_rate_1 < other.average_rate_1
+
+    def __gt__(self, other: 'Student'):
+        if not isinstance(other, Student):
+            raise TypeError("Wrong type for comparison with Student Class", other.__class__)
+        return self.average_rate_1 > other.average_rate_1
+
+    def __eq__(self, other: 'Student'):
+        if not isinstance(other, Student):
+            raise TypeError("Wrong type for comparison with Student Class", other.__class__)
+        return self.average_rate_1 == other.average_rate_1
 
     def average_rate(self):
         grade = 0
@@ -51,7 +66,6 @@ class Mentor:
         else:
             return 'Ошибка'
 
-
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -72,6 +86,20 @@ class Lecturer(Mentor):
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rate_1}"
 
+    def __lt__(self, other: 'Mentor'):
+        if not isinstance(other, Mentor):
+            raise TypeError("Wrong type for comparison with Mentor Class", other.__class__)
+        return self.average_rate_1 < other.average_rate_1
+
+    def __gt__(self, other: 'Mentor'):
+        if not isinstance(other, Mentor):
+            raise TypeError("Wrong type for comparison with Mentor Class", other.__class__)
+        return self.average_rate_1 > other.average_rate_1
+
+    def __eq__(self, other: 'Mentor'):
+        if not isinstance(other, Mentor):
+            raise TypeError("Wrong type for comparison with Mentor Class", other.__class__)
+        return self.average_rate_1 == other.average_rate_1
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -82,34 +110,76 @@ class Reviewer(Mentor):
         return f"Имя: {self.name}\nФамилия: {self.surname}"
 
 
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student_2 = Student('Kate', 'Bates', 'your_gender')
+best_student_2.courses_in_progress += ['Python']
 
 cool_lecturer = Lecturer("Other", "Buddy")
 cool_lecturer.courses_attached += ['Python']
 
+cool_lecturer_2 = Lecturer("Other_2", "Buddy_2")
+cool_lecturer_2.courses_attached += ['Python']
+
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
+cool_mentor_2 = Mentor('Some', 'Buddy')
+cool_mentor_2.courses_attached += ['Python']
+
 cool_reviewer = Reviewer("Other", "Buddy")
+cool_reviewer_2 = Reviewer("Other_2", "Buddy_2")
 
 best_student.rate_hw(cool_lecturer, 'Python', 9)
 best_student.rate_hw(cool_lecturer, 'Python', 10)
 best_student.rate_hw(cool_lecturer, 'Python', 8)
 
+best_student_2.rate_hw(cool_lecturer, 'Python', 9)
+best_student_2.rate_hw(cool_lecturer, 'Python', 10)
+best_student_2.rate_hw(cool_lecturer, 'Python', 8)
+
 cool_mentor.rate_hw(best_student, 'Python', 9)
 cool_mentor.rate_hw(best_student, 'Python', 4)
 cool_mentor.rate_hw(best_student, 'Python', 8)
 
-print(best_student.grades)
-print(cool_lecturer.grades)
+cool_mentor.rate_hw(best_student_2, 'Python', 7)
+cool_mentor.rate_hw(best_student_2, 'Python', 7)
+cool_mentor.rate_hw(best_student_2, 'Python', 6)
 
+cool_mentor_2.rate_hw(best_student, 'Python', 9)
+cool_mentor_2.rate_hw(best_student, 'Python', 4)
+cool_mentor_2.rate_hw(best_student, 'Python', 8)
+
+cool_mentor_2.rate_hw(best_student_2, 'Python', 7)
+cool_mentor_2.rate_hw(best_student_2, 'Python', 7)
+cool_mentor_2.rate_hw(best_student_2, 'Python', 6)
+
+print(best_student.grades)
+print(best_student_2.grades)
+print(cool_lecturer.grades)
+print(cool_lecturer_2.grades)
+
+best_student_2.average_rate()
+print(best_student_2.average_rate_1)
 best_student.average_rate()
 print(best_student.average_rate_1)
 cool_lecturer.average_rate()
 print(cool_lecturer.average_rate_1)
 
-print(cool_lecturer)
-print(cool_reviewer)
-print(best_student)
+# print(cool_lecturer)
+# print(cool_reviewer)
+# print(best_student)
+# print(best_student_2)
+print(best_student == best_student_2)
+print(best_student > best_student_2)
+print(best_student < best_student_2)
 
+Students = []
+
+print(Student())
+
+# for stud in Student:
+#     print(stud)
+#     Students.append(stud)
+# print(Students)
